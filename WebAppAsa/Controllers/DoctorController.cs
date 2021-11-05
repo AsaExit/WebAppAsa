f5936c4db255df85f7081369d7a3869557e9f32c
Åsa Jonsson <asaj71@gmail.com>
2021-11-05 15:06:51 +0100

    add doctor vader fevercheck

--- WebAppAsa/Controllers/DoctorController.cs
+++ WebAppAsa/Controllers/DoctorController.cs
@@ -8,9 +8,26 @@ namespace WebAppAsa.Controllers
 {
     public class DoctorController : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public IActionResult CheckFever(float temp)
+        {
+            if (temp != 0)
+            {
+
+                ViewBag.Msg = Models.FeverTemp.TestForFever(temp);
+                return View();
+            }
+            else
+            {
+                ViewBag.Msg = "Pls enter your temperaure and then hit Submit";
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult CheckFever()
         {
             return View();
         }
+
     }
 }
